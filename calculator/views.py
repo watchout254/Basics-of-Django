4b2a295ec09af5d52d10a5d96926913116b367ae
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-01-22 10:46:32 +0300

    Awooo

--- calculator/views.py
+++ calculator/views.py
@@ -6,8 +6,8 @@ def home(request):
     return render(request, 'home.html',{'name':'Daniel Mukenya'})
 
 def add(request):
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     res = val1 + val2
 
     return render(request, 'result.html', {'result': res})
